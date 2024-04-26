a2a408a7c8df6bea9aecdb2780917fe6c360619d
Nora Kauczor <n.kauczor@web.de>
2024-04-26 12:02:23 +0200

    write function for recipelist component

--- components/RecipeList.js
+++ components/RecipeList.js
@@ -1,1 +1,13 @@
-import recipes
+import { recipes } from "@/lib/recipes";
+import RecipeCard from "./RecipeCard";
+export default function RecipeList() {
+  return (
+    <ul>
+      {recipes.map((recipe) => (
+        <li>
+          <RecipeCard recipe={recipe} />
+        </li>
+      ))}
+    </ul>
+  );
+}
