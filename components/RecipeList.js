001911aa602c4bac6b39165e13a115941eb3fcc3
Nora Kauczor <n.kauczor@web.de>
2024-04-26 14:38:08 +0200

    finish US 1

--- components/RecipeList.js
+++ components/RecipeList.js
@@ -1,9 +1,15 @@
 import { recipes } from "@/lib/recipes";
 import RecipeCard from "./RecipeCard";
 import styled from "styled-components";
-export const StyledList = styled.ul`
+
+const StyledList = styled.ul`
   list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 4vh;
+  padding: 0 5vw;
 `;
+
 export default function RecipeList() {
   return (
     <StyledList>
