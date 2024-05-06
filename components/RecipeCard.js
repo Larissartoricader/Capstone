5e29a5d586798f2f35457e18fcf69a6ea4feb103
Natascha Tenhagen <nattenhagen@googlemail.com>
2024-05-06 23:54:58 +0200

    add global styling

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -5,24 +5,19 @@ import { BookmarkIcon } from "./BookmarkIcon";
 
 const StyledList = styled.ul`
   list-style: none;
-  padding-left: 2vw;
+  padding: 0;
 `;
 
 const StyledArticle = styled.article`
   border: 1px solid black;
   display: flex;
-  align-items: center;
+  flex-direction: column;
   margin-left: 1vw;
   box-sizing: border-box;
-`;
-
-const StyledHeadline = styled.h2`
-  padding-left: 2vw;
+  position: relative;
 `;
 
 const StyledListHeader = styled.p`
-  padding-left: 2vw;
-  margin-bottom: vh;
   font-weight: bold;
   font-size: 1.2em;
 `;
@@ -31,22 +26,13 @@ const StyledListItems = styled.li`
   margin-bottom: 0.5em;
 `;
 
-const StyledHeadlineWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
 const StyledCardWrapper = styled.div`
-  flex: 1;
   display: flex;
   flex-direction: column;
-  width: 50%;
+  padding: 12px;
 `;
 
-const StyledImageWrapper = styled.div`
-  width: 50%;
-`;
+const StyledImageWrapper = styled.div``;
 
 const StyledLink = styled.link`
   text-decoration: none;
@@ -55,6 +41,16 @@ const StyledLink = styled.link`
   background-color: #eee;
   border-radius: 5px;
   transition: background-color 0.3s ease;
+`;
+
+const StyledBookmarkIcon = styled.div`
+  position: absolute;
+  top: -12px;
+  right: 0;
+`;
+const StyledLinkWrapper = styled.div`
+  text-align: right;
+  width: 100%;
 `;
 
 export default function RecipeCard({
@@ -68,28 +64,39 @@ export default function RecipeCard({
         <Image
           src="https://images.unsplash.com/photo-1564278453360-c65eda0a200e?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           // fill={true}
-          height={250}
-          width={250}
+          style={{ width: "100%" }}
+          height={200}
+          width={200}
           objectFit="contain"
           alt="bottle of rum e.g. remedy"
         ></Image>
       </StyledImageWrapper>
+      <StyledBookmarkIcon>
+        <BookmarkIcon
+          onHandleBookmarkedIcon={onHandleBookmarkedIcon}
+          bookmarkedRecipesIDs={bookmarkedRecipesIDs}
+          recipe={recipe}
+        />
+      </StyledBookmarkIcon>
       <StyledCardWrapper>
-        <StyledHeadlineWrapper>
-          <StyledHeadline>{recipe.title}</StyledHeadline>
-          <BookmarkIcon
-            onHandleBookmarkedIcon={onHandleBookmarkedIcon}
-            bookmarkedRecipesIDs={bookmarkedRecipesIDs}
-            recipe={recipe}
-          />
-        </StyledHeadlineWrapper>
+        <h2>{recipe.title}</h2>
         <StyledListHeader>Symptoms:</StyledListHeader>
         <StyledList>
           {recipe.symptoms.map((symptom) => (
             <StyledListItems key={symptom}>{symptom}</StyledListItems>
           ))}
         </StyledList>
-        <Link href={`/${recipe.id}`}>Read More</Link>
+        <StyledLinkWrapper>
+          <Link
+            style={{
+              color: "black",
+              textUnderlineOffset: "4px",
+            }}
+            href={`/${recipe.id}`}
+          >
+            Read More →
+          </Link>
+        </StyledLinkWrapper>
       </StyledCardWrapper>
     </StyledArticle>
   );
--- components/RecipeList.js
+++ components/RecipeList.js
@@ -3,10 +3,19 @@ import styled from "styled-components";
 
 const StyledList = styled.ul`
   list-style: none;
-  display: flex;
-  flex-direction: column;
-  gap: 4vh;
-  padding: 0 5vw;
+  display: grid;
+  column-gap: 20px;
+  row-gap: 40px;
+  grid-template-columns: 1fr;
+  padding: 0 28px;
+
+  @media only screen and (min-width: 425px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media only screen and (min-width: 768px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
 `;
 
 const WhiteSpace = styled.div`
