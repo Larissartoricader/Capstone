eb2a4bb0394c7c0e82114e59122caa9965b8fdff
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-05-14 17:03:26 +0200

    add New Font Family

--- components/GlobalStyles.js
+++ components/GlobalStyles.js
@@ -1,11 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Be+Vietnam:wght@500&display=swap');
+
   body {
     margin: 0;
     padding: 0;
     background-color: #c8ecb8;
-    font: Lora;
+    font-family: 'Be Vietnam Pro', sans-serif;
     
   }
 `;
--- pages/api/recipes/index.js
+++ pages/api/recipes/index.js
@@ -2,30 +2,30 @@ import dbConnect from "@/db/connect";
 import Recipe from "@/db/models/Recipe";
 
 export default async function handler(request, response) {
-  try {
-    await dbConnect();
-    if (request.method === "GET") {
-      const recipes = await Recipe.find();
-      return response.status(200).json(recipes);
-    }
-  } catch (error) {
-    console.error("Error while Fetching", error);
-    return response.status(500).json({ error: "Internal Server Error!" });
-  }
+	try {
+		await dbConnect();
+		if (request.method === "GET") {
+			const recipes = await Recipe.find();
+			return response.status(200).json(recipes);
+		}
+	} catch (error) {
+		console.error("Error while Fetching", error);
+		return response.status(500).json({ error: "Internal Server Error!" });
+	}
 
-  if (request.method === "POST") {
-    await dbConnect();
-    try {
-      const recipeData = request.body;
-      if (!recipeData.hasOwnProperty("editable")) {
-        recipeData.editable = true;
-      }
-      await Recipe.create(recipeData);
+	if (request.method === "POST") {
+		await dbConnect();
+		try {
+			const recipeData = request.body;
+			if (!recipeData.hasOwnProperty("editable")) {
+				recipeData.editable = true;
+			}
+			await Recipe.create(recipeData);
 
-      response.status(201).json({ status: "Recipe created." });
-    } catch (error) {
-      console.log(error);
-      response.status(400).json({ error: error.message });
-    }
-  }
+			response.status(201).json({ status: "Recipe created." });
+		} catch (error) {
+			console.log(error);
+			response.status(400).json({ error: error.message });
+		}
+	}
 }
