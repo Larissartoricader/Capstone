15c7f41b211a14bbb2b967098a43b02c348d4fe8
Arne Witteler <witteler@arnenoah.de>
2024-05-16 16:45:09 +0200

    rebase

--- db/models/Recipe.js
+++ db/models/Recipe.js
@@ -8,7 +8,11 @@ const RecipeSchema = new Schema({
 	preparation: { type: String, required: true },
 	usage: { type: String, required: true },
 	symptoms: [{ type: String, required: true }],
+<<<<<<< HEAD
 	image: { type: String },
+=======
+	image: { type: String, required: false },
+>>>>>>> 0182b4e (add changes)
 	editable: { type: Boolean, required: false },
 });
 
