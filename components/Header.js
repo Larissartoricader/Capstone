a4cbe12cceb82ff1cc47db4cd50cce4675050dfd
Nora Kauczor <n.kauczor@web.de>
2024-05-22 15:23:03 +0200

    install react icons

--- components/Header.js
+++ components/Header.js
@@ -5,20 +5,15 @@ import Login from "./Login"
 const HeaderWrapper = styled.article`display: flex;
 justify-content: space-evenly;
 align-items: center;
-gap: 65%;
+gap: 50%;
 width: 100%; color: white; background-color: var(--primary-button-and-header-color);`
 
 const AppName = styled.p`font-family: var(--herbie-font); font-size: 130%;`
 
-const LoginWrapper = styled.p`
-
-`
 
 export function Header(){
     return (<HeaderWrapper>
-            <LoginWrapper>
                 <Login/>
-            </LoginWrapper>
             <AppName>herbie</AppName>
             </HeaderWrapper>)
 }
--- components/Login.js
+++ components/Login.js
@@ -1,22 +1,23 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 import styled from "styled-components";
+import { MdLogin, MdLogout } from "react-icons/md";
 
-const LoginButton = styled.button`color: white; background-color: hotpink;
-border: none;`
+const LoginButton = styled.button`
+color: white; background-color: var(--primary-button-and-header-color);
+border: none; font-family: var(--geberal-font); font-size: 100%;`
 
 export default function Login() {
   const { data: session } = useSession();
   if (session) {
     return (
       <>
-        <LoginButton onClick={() => signOut()}>Sign out</LoginButton>
+        <LoginButton onClick={() => signOut()}>Logout <MdLogout /></LoginButton>
       </>
     );
   }
   return (
     <>
-      Not signed in <br />
-      <LoginButton onClick={() => signIn()}>Sign in</LoginButton>
+      <LoginButton onClick={() => signIn()}>Login <MdLogin /></LoginButton>
     </>
   );
 }
