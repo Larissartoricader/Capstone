cd65d5c37be42d802d2b88f10f9a4756422e89ef
Nora Kauczor <n.kauczor@web.de>
2024-05-22 15:01:27 +0200

    rename login component to login and delete the following from its jsx: Signed in as {session.user.email}

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,20 @@
 import styled from "styled-components"
+import Login from "./Login"
 
 
-const HeaderContainer = styled.div`display: flex;`
+const HeaderContainer = styled.article`display: flex;
+justify-content: space-evenly;
+gap: 65%;
+width: 100%; color: white; background-color: var(--primary-button-and-header-color);`
+const Name = styled.p`font-family: var(--herbie-font); font-size: 130%;`
+const LoginButton = styled.button`
+color: white; background-color: var(--primary-button-and-header-color);
+border: none;
+`
 
 export function Header(){
-    
-        <HeaderContainer></HeaderContainer>
+    return (<HeaderContainer>
+            <LoginButton type="button"><Login/></LoginButton>
+            <Name>herbie</Name>
+            </HeaderContainer>)
 }
