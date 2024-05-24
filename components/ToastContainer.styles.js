98eca3a41ab2aef1bb6bbff7621c831f965c84f2
Arne Witteler <witteler@arnenoah.de>
2024-05-24 09:57:29 +0200

    delete unnecessary styling

--- components/ToastContainer.styles.js
+++ components/ToastContainer.styles.js
@@ -16,18 +16,4 @@ export const CustomToastContainer = styled(ToastContainer)`
   .Toastify__progress-bar {
     background-color: #ffc107;
   }
-
-  .Toastify__toast-icon {
-    color: #ffc107; /* Customize the color of the icon */
-  }
-
-  .Toastify__close-button {
-    color: #ffffff; /* Customize the color of the close button */
-    opacity: 1;
-  }
-
-  .Toastify__close-button:hover,
-  .Toastify__close-button:focus {
-    color: #ffc107; /* Change color on hover or focus */
-  }
 `;
